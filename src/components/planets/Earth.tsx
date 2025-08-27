e01a73ec6f5e2a5e0b03cca441b1c5ba8b1c6693
Tihomir <tihomirkg91@gmail.com>
2025-08-27 14:59:57 +0200

    a

--- src/components/planets/Earth.tsx
+++ src/components/planets/Earth.tsx
@@ -15,7 +15,8 @@ export const Earth = (props: Omit<PlanetProps, 'planetData'>) => {
       'radial-gradient(circle at 30% 30%, #b0e0e6 0%, #5f9ea0 30%, #32cd32 60%, #228b22 100%)',
     kickedBoxShadow:
       '0 0 35px rgba(135, 206, 235, 0.8), inset -8px -8px 15px rgba(50, 205, 50, 0.5)',
-    texture: 'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/world.topo.bathy.200412.3x5400x2700.jpg',
+    texture:
+      'https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/world.topo.bathy.200412.3x5400x2700.jpg',
   };
 
   return <Planet {...props} planetData={earthData} />;
